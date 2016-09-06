3dd8773da9a926fdd3dd288534e7d94770f0ec0a
joker1007 <kakyoin.hierophant@gmail.com>
2016-09-06 15:25:39 +0900

    Refactor public methods

--- lib/emrakul.rb
+++ lib/emrakul.rb
@@ -7,8 +7,37 @@ require 'sshkit'
 
 module Emrakul
   class << self
-    def run_emr(
+    def run(
       configs,
+      gemfile_path,
+      ec2_key_path,
+      aws_access_key_id: nil,
+      aws_secret_access_key: nil,
+      aws_region: nil,
+      user: "hadoop",
+      emr_config: {},
+      embulk_path: `which embulk`.chomp,
+      additional_scripts: [],
+      additional_uploads: []
+    )
+      job_flow_id = setup_cluster(
+        gemfile_path, ec2_key_path,
+        aws_access_key_id: aws_access_key_id, aws_secret_access_key: aws_secret_access_key, aws_region: aws_region,
+        user: user,
+        emr_config: emr_config,
+        embulk_path: embulk_path,
+        additional_scripts: additional_scripts,
+        additional_uploads: additional_uploads
+      )
+
+      run_on_emr(
+        configs, job_flow_id, ec2_key_path,
+        aws_access_key_id: aws_access_key_id, aws_secret_access_key: aws_secret_access_key, aws_region: aws_region,
+        user: user
+      )
+    end
+
+    def setup_cluster(
       gemfile_path,
       ec2_key_path,
       aws_access_key_id: nil,
@@ -22,6 +51,31 @@ module Emrakul
     )
       raise "Need embulk" if embulk_path.nil? || embulk_path.empty?
 
+      client = emr_client(
+        aws_access_key_id: aws_access_key_id,
+        aws_secret_access_key: aws_secret_access_key,
+        aws_region: aws_region
+      )
+
+      job_flow_id = create_cluster(client: client, emr_config: emr_config)
+      master_instance = client.list_instances(cluster_id: job_flow_id, instance_group_types: ["MASTER"]).instances[0]
+
+      setup_embulk(user, master_instance, ec2_key_path, embulk_path, gemfile_path,
+                   additional_scripts: additional_scripts, additional_uploads: additional_uploads)
+
+      job_flow_id
+    end
+
+    def run_on_emr(
+      configs,
+      job_flow_id,
+      ec2_key_path,
+      aws_access_key_id: nil,
+      aws_secret_access_key: nil,
+      aws_region: nil,
+      user: "hadoop",
+      shutdown: true
+    )
       configs = Array(configs)
       tfs = []
       config_paths = configs.each_with_object([]) do |config, arr|
@@ -44,23 +98,17 @@ module Emrakul
         aws_region: aws_region
       )
 
-      job_flow_id = run_cluster(client: client, emr_config: emr_config)
       master_instance = client.list_instances(cluster_id: job_flow_id, instance_group_types: ["MASTER"]).instances[0]
-
-      setup_embulk(user, master_instance, ec2_key_path, embulk_path, gemfile_path,
-                   additional_scripts: additional_scripts, additional_uploads: additional_uploads)
 
       config_paths.each do |config_path|
         run_embulk(user, master_instance, ec2_key_path, config_path)
       end
     ensure
       tfs.each(&:close!) if tfs
-      client.terminate_job_flows(job_flow_ids: [job_flow_id]) if job_flow_id
+      client.terminate_job_flows(job_flow_ids: [job_flow_id]) if job_flow_id && shutdown
     end
 
-    private
-
-    def run_cluster(client:, emr_config:)
+    def create_cluster(client:, emr_config:)
       emr_config =
         default_emr_options
         .tap { |o| o.delete(:applications) if emr_config[:applications] }
@@ -90,7 +138,13 @@ module Emrakul
           execute :embulk, "bundle", "install"
 
           additional_uploads.each do |f|
-            upload! f, File.join(workspace_dir, File.basename(f))
+            if f.is_a?(Hash)
+              f.each do |k, v|
+                upload! k, File.join(workspace_dir, v)
+              end
+            else
+              upload! f, File.join(workspace_dir, File.basename(f))
+            end
           end
 
           additional_scripts.each do |script|
@@ -114,6 +168,8 @@ module Emrakul
         end
       end
     end
+
+    private
 
     def default_emr_options
       {
